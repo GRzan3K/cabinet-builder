758916957ee66da7fcbecabb08190900117af4e3
filipwoz <filipwozniakowski974@gmail.com>
2026-08-23 23:31:37 +0200

    feat: calculate basic cabinet panels

--- app.js
+++ app.js
@@ -1,31 +1,48 @@
+const form = document.getElementById("cabinet-form");
+
 const widthInput = document.getElementById("width");
-const thicknessInput = document.getElementById("thickness");
 const heightInput = document.getElementById("height");
 const depthInput = document.getElementById("depth");
+const shelvesInput = document.getElementById("shelves");
 
-const calculateButton =
-    document.getElementById("calculate");
+const result = document.getElementById("results");
 
-const result =
-    document.getElementById("result");
-
-
-calculateButton.addEventListener("click", function () {
-
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    
     const width = Number(widthInput.value);
-    const thickness = Number(thicknessInput.value)
     const height = Number(heightInput.value);
     const depth = Number(depthInput.value);
+    const shelves = Number(shelvesInput.value);
+    
+    const thickness = 18;
+    const shelfOffset = 20;
 
     const insideWidth = width - thickness * 2;
 
     const sideHeight = height;
     const sideDepth = depth;
+   
+    const topWidth = insideWidth;
+    const topDepth = depth;
 
-    const sidesArea = sideHeight * sideDepth * 2;
-    const sidesAreaM2 = sidesArea / 1000000;
+    const bottomWidth = insideWidth;
+    const bottomDepth = depth;
 
-    result.textContent =
-        "Szerokość wewnętrzna: " + insideWidth + " mm";
+    const shelfWidth = insideWidth;
+    const shelfDepth = depth - shelfOffset;
+
+    result.innerHTML = `
+    <h2>Lista elementów</h2>
+
+    <p>Boki: 2 szt. - ${sideHeight} x ${sideDepth} mm</p>
+
+    <p>Góra: 1 szt. - ${topWidth} x ${topDepth} mm</p>
+
+    <p>Dół: 1 szt. - ${bottomWidth} x ${bottomDepth} mm</p>
+
+    <p>Półki: ${shelves} szt. - ${shelfWidth} x ${shelfDepth} mm</p>
+    
+    `;
 
 });
